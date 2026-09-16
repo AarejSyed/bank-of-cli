d7b5cb6984ed9e58dbf2c27ba4afcf8b77f57736
AarejSyed <159865376+AarejSyed@users.noreply.github.com>
2026-09-16 15:32:23 -0500

    Regex check for account PIN is now case-sensitive; saves miniscule amount of resources without changing functionality

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE account (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
     pin TEXT NOT NULL
-        CHECK(pin ~* '^[0-9]{4}$'),                     -- Account PIN must be 4 digits long
+        CHECK(pin ~ '^[0-9]{4}$'),                      -- Account PIN must be 4 digits long
     balance NUMERIC(15, 2) NOT NULL DEFAULT 0.00
         CHECK(balance >= 0.00)                          -- Account balance cannot be negative
 );
